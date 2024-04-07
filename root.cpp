97bc41ec77af87a1806426036aeee84138eff1d9
Young <81169774+GrowingUnderTheTree@users.noreply.github.com>
2024-04-07 20:34:15 +0800

    Cubic root equation with newton rhamson method

--- root.cpp
+++ root.cpp
@@ -1,17 +1,48 @@
 #include <iostream>
 
-int main() {
+void cube() {
     long double input;
     long double initial = 1;
     long double up, down;
-    std::cout << "Enter the number to calculate square root : "<< std::endl;
-    std::cin >> input ;
+    std::cout << "Enter number to calculate cube root : " << std::endl;
+    std::cin >> input;
     for(int i = 1; i < 11; i++){
+        up = (initial * initial * initial) - input;
+        down = 3 * (initial * initial);
+        initial = initial - (up/down);
+        std::cout.precision(50);
+        std::cout << "round " << i << " answer = " << initial << std::endl;
+}
+}
+void square() {
+  long double input;
+    long double initial = 1;
+    long double up, down;
+    std::cout << "Enter the number to calculate square root : "<< std::endl;
+    //newton's method
+    std::cin >> input ;
+    for(int i = 1; i < 1001; i++){
         up = (initial * initial) - input;
         down = 2 * initial;
         initial = initial - (up/down);
         std::cout.precision(63);
         std::cout << "round " << i << " answer = " << initial << std::endl;
     }
+    //to calculate square root
+}
+
+int main() {
+    long double input;
+    long double initial = 1;
+    long double up, down;
+    std::cout << "Enter number to calculate cube root : " << std::endl;
+    std::cin >> input;
+    for(int i = 1; i < 1001; i++){
+        up = (initial * initial * initial) - input;
+        down = 3 * (initial * initial);
+        initial = initial - (up/down);
+        std::cout.precision(12);
+     }
+    std::cout <<" answer = " << initial << std::endl;
     return 0;
 }
